4c56bdd19d627e14caac682844c49a9b9f143beb
amaanansari26 <amn.github@gmail.com>
2023-02-23 16:31:53 +0530

    swagger ui config in json

--- src/swaggerUiConfig.mjs
+++ src/swaggerUiConfig.mjs
@@ -1,25 +1,113 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Taxi APP",
-        version: "1.0.0",
-        description:
-          "Taxi app of excellence technologies",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
+const swaggerSpecs = {
+  "openapi": "3.0.0",
+  "info": {
+    "title": "Example API",
+    "version": "1.0.0",
+    "description": "An example API with Swagger"
+  },
+  "servers":[{
+      "url": `${process.env.BASE_URL}/api`,
+      "description": "Local development server"
+    }],
+  "paths": {
+    "/user/create": {
+      "post": {
+        "summary": "This api will create new user",
+        "tags": ["Users"],
+        "requestBody": {
+          "required": true,
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "required": [
+                  "username",
+                  "password",
+                  "email",
+                  "firstname",
+                  "lastname"
+                ],
+                "properties": {
+                  "username": {
+                    "type": "string"
+                  },
+                  "password": {
+                    "type": "string"
+                  },
+                  "email": {
+                    "type": "string"
+                  },
+                  "phoneNo": {
+                    "type": "string"
+                  },
+                  "firstname": {
+                    "type": "string"
+                  },
+                  "lastname": {
+                    "type": "string"
+                  }
+                }
+              }
+            }
+          }
         },
-      },
-      servers: [
-        {
-          url: "http://localhost:3000/api",
-        },
-      ],
+        "responses": {
+          "200": {
+            "description": "New user is created.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object"
+                }
+              }
+            }
+          },
+          "500": {
+            "description": "Some server error"
+          }
+        }
+      }
     },
-    apis: ['./src/routes/*.mjs']
-  };
-  
-  const swaggerSpecs = swaggerJsdoc(options);
-  export default swaggerSpecs;
+    "/user/login": {
+      "post": {
+        "summary": "This api will login the user",
+        "tags": ["Users"],
+        "requestBody": {
+          "required": true,
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "required": ["email", "password"],
+                "properties": {
+                  "email": {
+                    "type": "string"
+                  },
+                  "password": {
+                    "type": "string"
+                  }
+                }
+              }
+            }
+          }
+        },
+        "responses": {
+          "200": {
+            "description": "user token is generated.",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object"
+                }
+              }
+            }
+          },
+          "500": {
+            "description": "Some server error"
+          }
+        }
+      }
+    }
+  }
+}
+export default swaggerSpecs;
