db47918b3376d88346126c2f6495241ffeb82639
anmolint <abaloni02@gmail.com>
2023-02-23 16:56:11 +0530

    added-authentication-middleware

--- src/controllers/user.mjs
+++ src/controllers/user.mjs
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/user.mjs';
 import httpErrors from 'http-errors';
 import UserProfile from '../models/userProfile.mjs';
-import * as Helpers from '../helpers/validators.mjs';
+import * as Helpers from '../helpers/validationHelpers.mjs';
 
 const { JWT_SECRET_KEY } = process.env;
 const INVALID_CRED = 'Invalid Email or Password';
@@ -11,27 +11,9 @@ const INVALID_CRED = 'Invalid Email or Password';
 export const create = async (req, res) => {
   const { username, password, email, phoneNo, firstname, lastname } = req.body;
 
-  if (!Helpers.isValidPhoneNo(phoneNo)) {
-    return res.status(400).json({ success: false, message: 'Please enter a valid phone number.' });
-  }
-
-  if (!Helpers.isValidEmail(email)) {
-    return res.status(400).json({ success: false, message: 'Please enter a valid email address.' });
-  }
-
-  const userExist = await Helpers.userExists(email);
-  const phoneNoExist = await Helpers.phoneNoExists(phoneNo);
-
-  if (userExist) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'User with given Email address already registered.' });
-  }
-
-  if (phoneNoExist) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'User with given Phone Number already registered.' });
+  const { success, message } = await Helpers.validateUserInputs(email, phoneNo);
+  if (!success) {
+    return res.status(400).json({ success, message });
   }
 
   const hashedPassword = await bcrypt.hash(password || '', 10);
@@ -99,15 +81,28 @@ export const login = async (req, res) => {
 export const updateUser = async (req, res) => {
   const { userId } = req.params;
   const update = req.body;
-  if (update.phoneNo && !Helpers.isValidPhoneNo(update?.phoneNo)) {
-    return res.status(400).json({ message: 'Please enter a valid phone number.' });
-  }
 
-  if (update.email && !Helpers.isValidEmail(update?.email)) {
-    return res.status(400).json({ message: 'Please enter a valid email address.' });
+  const { success, message } = await Helpers.validateUserInputs(update.email, update.phoneNo);
+  if (!success) {
+    return res.status(400).json({ success, message });
   }
   //update user with given data
   await User.update({ update, where: { id: userId } });
+
+  const user = await User.findOne({
+    where: { id: userId },
+    include: [{ model: UserProfile }],
+  });
+
+  res.status(200).json({ success: true, user });
+};
+
+export const updateUserProfile = async (req, res) => {
+  const { userId } = req.params;
+  const update = req.body;
+
+  //update user with given data
+  await UserProfile.update({ update, where: { userId: userId } });
 
   const user = await User.findOne({
     where: { id: userId },
--- src/middleware.mjs
+++ src/middleware.mjs
@@ -1,7 +1,41 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.mjs';
+import UserProfile from '../models/userProfile.mjs';
+
+const { JWT_SECRET_KEY } = process.env;
+
 // eslint-disable-next-line no-unused-vars
 export const globalErrorHandler = (error, req, res, _next) => {
   console.error(`Err in ${req.url}`, error);
   return res
     .status(error.status || 500)
     .json({ error: { success: false, message: error.message || 'An expected error occurred' } });
+};
+
+export const authenticateUser = async (req, res, next) => {
+  if (!req.header('authorization')) {
+    return res
+      .status(401)
+      .json({ success: false, message: 'Token missing or malformed Please pass authorization' });
+  }
+  const token = req.header('authorization').replace(/^Bearer\s+/, '');
+  const decode = jwt.verify(token, JWT_SECRET_KEY);
+  if (decode) {
+    const { sub: userId } = decode;
+    const user = await User.findOne({
+      where: { id: userId },
+      include: [{ model: UserProfile }],
+    });
+    if (!user) {
+      return res.status(403).json({
+        success: false,
+        message: 'Unauthorized you are not allowed to access the resource',
+      });
+    }
+    req.user = user;
+    next();
+  }
+  return res
+    .status(401)
+    .json({ success: false, message: 'Token missing or malformed Please pass authorization' });
 };
